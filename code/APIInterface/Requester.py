506b09bb15759d4e38168da55573414d4e73d04a
Netwave <3danimanimal@gmail.com>
2015-03-23 23:12:11 +0100

    data added

--- code/APIInterface/Requester.py
+++ code/APIInterface/Requester.py
@@ -2,4 +2,31 @@ __author__ = 'Netwave'
 
 
 
+import requests as rq
 
+
+class RiotRequester:
+    northamerica = 'na'
+    europewest = 'euw'
+    europenortheast = 'eune'
+    brazil = 'br'
+    turkey = 'tr'
+    russia = 'ru'
+    latinamericanorth = 'lan'
+    latinamericansouth = 'las'
+    oceania = 'oce'
+
+    baseurl = 'https://{zone}.api.pvp.net'
+
+    def __init__(self, requester):
+        self.requester = requester
+
+    def test(self, zone):
+        testurl = '/api/lol/{region}/v1.4/summoner/by-name/{summonerNames}'.format(region=RiotRequester.europewest, summonerNames='n3twave')
+        payload = {'key': open("KeyFile").read()}
+        fullurl = RiotRequester.baseurl.format(region=RiotRequester.europewest)+testurl
+        r = self.requester.get(fullurl, payload)
+        return r.json()
+
+    def getUserHistory(self):
+        pass
